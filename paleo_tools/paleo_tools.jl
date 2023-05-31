6dc6f565f2596a5307fda7f612e6ff156e028965
Sergio Pérez-Montero <98036930+sperezmont@users.noreply.github.com>
2023-05-31 09:42:31 +0200

    Update paleo_tools.jl

--- paleo_tools/paleo_tools.jl
+++ paleo_tools/paleo_tools.jl
@@ -2,16 +2,22 @@
 	convert_d18O_to_temp(d180; a)
 converts d18O data to temperature anomaly through
             d18O = aT + b       (based on Cuffey et al. (1994, 1995))
-## Attributes
-`d18O` Vector or value to convert
-`a` slope
+        or
+            T = 16.5 - 4.3 * d18O + 0.14 * d18O²       (based on Epstein et al. (1953))
+### Attributes
+* `d18O` Vector or value to convert
+* `a` slope
+* `mode` equation to use
 
-## Default values
+### Default values
 Default value of a is selected on a calibration of Barker et al., 2011 d18O values to -20ºC anomaly in LGM
-
 """
-function convert_d18O_to_temp(d18O::Any; a::Real=0.3)
-    b = 31.47 * a - 34.87
-    T = (d18O .- b) ./ a 
+function convert_d18O_to_temp(d18O::Any; a::Real=0.3, mode="barker")
+    if mode == "barker" # Cuffey et al. (1994, 1995)
+        b = 31.47 * a - 34.87
+        T = (d18O .- b) ./ a 
+    elseif  mode == "epstein"   # Epstein (1953)
+        T = 16.5 .- 4.3 .* d18O .+ 0.14 .* (d18O .^2)
+    end
     return T
 end
