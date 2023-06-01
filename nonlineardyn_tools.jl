fb89439d407f03269bb54e514ff7e632c728117d
Sergio Pérez-Montero <98036930+sperezmont@users.noreply.github.com>
2023-06-01 09:51:45 +0200

    Update nonlineardyn_tools.jl

--- nonlineardyn_tools.jl
+++ nonlineardyn_tools.jl
@@ -1,11 +1,12 @@
 """
-    calc_recurrence(d; tol)
-calculates the recurrence matrix of a time series `d`
+    calc_recurrence(u; tol)
+calculates the recurrence matrix of the vector `u` that represents 
+the time series of the state of a dynamic process
 """
-function calc_recurrence(d::Vector; tol::Real=0.1)
-    R = Matrix{Any}(undef, length(d), length(d))
-    for i in eachindex(d), j in eachindex(d)
-        (abs(d[i] - d[j]) <= tol) ? (R[i, j] = 1.0) : (R[i, j] = 0.0) 
+function calc_recurrence(u::Vector; tol::Real=0.1)
+    R = Matrix{Any}(undef, length(u), length(u))
+    for i in eachindex(u), j in eachindex(u)
+        (norm(u[i] - u[j]) <= tol) ? (R[i, j] = 1.0) : (R[i, j] = 0.0) 
     end
     return R
 end
